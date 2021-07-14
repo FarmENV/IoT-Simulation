15c39835c5be9ff799b81df1dbf80a37fdf08582
Saul Uriel <sauluriel1402@gmail.com>
2021-07-14 17:44:17 -0600

    Automatic posts working

--- arduino.py
+++ arduino.py
@@ -7,8 +7,6 @@ option = True
 temp = 30
 food = 3000
 
-hash_password = "ECF389B6DDCF5D10D1D494924300A2ADC23AFAFD03FE9669C27774ED2895B6EC"
-
 arduinoId = "arduinoid1"
 
 while option:
@@ -24,17 +22,17 @@ while option:
     airQuality=(random.choice(["Good","Bad"]))
 
     """ The request for the data """
-    REQUEST_URL = f"http://127.0.0.1:5000/option/{hash_password}/?arduinoId={arduinoId}&temp={temp}&food={food}&airQuality={airQuality}"
+    REQUEST_URL = f"http://127.0.0.1:5000/option/?arduinoId={arduinoId}&temp={temp}&food={food}&airQuality={airQuality}"
     _request = requests.get(REQUEST_URL)
     print(_request.text)
 
 
     """ Data obtained from the json """
-    data = json.loads(_request.text)
+    """ data = json.loads(_request.text)
     data_id = data['arduinoId']
     data_temp = data['temp']
     data_food = data['food']
-    data_air = data['airQuality']
+    data_air = data['airQuality'] """
 
 
     time.sleep(3)
